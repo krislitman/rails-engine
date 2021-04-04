5ee96459ce0747a27e5a02d88fb4e556201a3405
Kris Litman <kris.d.litman@gmail.com>
2021-04-04 00:17:37 -0400

    Section 2 done

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
-    items = ItemFacade.search(params[:name])
-    render json: ItemSerializer.new(items)
+    case
+    when params[:name] && params[:min_price] && params[:max_price]
+      render json: { message: "Wrong parameters" }, status: 404
+    when params[:name] && params[:min_price]
+      render json: { message: "Wrong parameters" }, status: 404
+    when params[:name] && params[:max_price]
+      render json: { message: "Wrong parameters" }, status: 404
+    else
+      items = ItemFacade.search(search_params)
+      render json: ItemSerializer.new(items)
+    end
+  end
+
+  private
+
+  def search_params
+    params.permit(:name, :min_price, :max_price)
   end
 end
